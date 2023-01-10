44c0ac4330ead3e0e1abe7d34adc676260d0993c
Jahongir2109 <jahongirachilov004@gmail.com>
2023-01-10 21:31:02 +0500

    fixed by test files

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -6,9 +6,9 @@ public class CopiedNames {
         int variableTwoThousand = 2000;
         int value = 1000;
         int variableFourThousand = 4000;
-        int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
+        int valueTwo = 3000;
+        int variableFiveThousand = 5000;
+        variableFiveThousand = 7000;
         int valueNineThousand = 90000;
     }
 }
